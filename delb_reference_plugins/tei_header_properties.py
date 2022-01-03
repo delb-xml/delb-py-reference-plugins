c52c15b65484b2a662a7736fd321283e5f80ef55
Frank Sachsenheim <funkyfuture@riseup.net>
2022-01-03 17:25:54 +0100

    Adjustments for delb 0.3

--- delb_reference_plugins/tei_header_properties.py
+++ delb_reference_plugins/tei_header_properties.py
@@ -1,15 +1,28 @@
 # note that nothing is imported from the top-level `delb` module as it would
 # trigger the loading of plugins, yet the plugins in this module hasn't been
 # registered yet
+from types import SimpleNamespace
+from typing import Any, Dict
 
-from _delb.plugins import plugin_manager, DocumentExtensionHooks
+from _delb.plugins import plugin_manager, DocumentMixinHooks
+
+
+# TODO a simple storage-related plugin makes more sense in light of the sublass.py
+#      contained example
 
 
 @plugin_manager.register_document_extension
-class TEIHeader(DocumentExtensionHooks):
-    def _init_config(self, config_args):
-        super()._init_config(config_args)
-        self.tei_header = TEIHeaderProperties(self)
+class TEIHeader(DocumentMixinHooks):
+    @classmethod
+    def _init_config(cls, config: SimpleNamespace, kwargs: Dict[str, Any]):
+        config.tei_header = SimpleNamespace(
+            yelling=kwargs.pop("tei_header_yelling", False)
+        )
+        super()._init_config(config, kwargs)
+
+    @property
+    def tei_header(self):
+        return TEIHeaderProperties(self)
 
 
 class TEIHeaderProperties:
@@ -27,6 +40,9 @@ class TEIHeaderProperties:
 
     @property
     def title(self):
-        return self.document.css_select(
+        result = self.document.css_select(
             "teiHeader fileDesc titleStmt title"
         ).first.full_text.strip()
+        if self.document.config.tei_header.yelling:
+            result = result.upper()
+        return result
--- test.py
+++ test.py
@@ -1,13 +1,33 @@
 import pkg_resources
+from types import SimpleNamespace
 
+from _delb.nodes import TagNode
 from delb import Document
+
+
+OFFICE_NS = "urn:oasis:names:tc:opendocument:xmlns:office:1.0"
+ODT_MIMETYPE = "application/vnd.oasis.opendocument.text"
+
+
+class OpenDocumentText(Document):
+    @staticmethod
+    def __class_test__(root: TagNode, config: SimpleNamespace) -> bool:
+        if root.namespace != OFFICE_NS:
+            return False
+
+        if root.local_name != "document":
+            return False
+
+        try:
+            return root.attributes[OFFICE_NS:"mimetype"] == ODT_MIMETYPE
+        except KeyError:
+            return False
 
 
 def test_custom_loader():
     url = "remote://the_book_of_brian.xml"
-    document = Document(url)
+    root = Document(url).root
 
-    root = document.root
     assert root.local_name == "document"
     assert root["url"] == url
 
@@ -19,6 +39,16 @@ def test_header_properties():
 
     assert document.tei_header.title == "Das erste dadaistische Manifest"
     assert document.tei_header.authors == ["Ball, Hugo"]
+
+
+def test_subclass():
+    stub = f'<document xmlns="{OFFICE_NS}" mimetype="{ODT_MIMETYPE}"/>'
+    assert isinstance(Document(stub), OpenDocumentText)
+
+    stub = stub.replace("open", "closed")
+    document = Document(stub)
+    assert isinstance(document, Document)
+    assert not isinstance(document, OpenDocumentText)
 
 
 if __name__ == "__main__":
@@ -33,4 +63,5 @@ if __name__ == "__main__":
 
     test_custom_loader()
     test_header_properties()
+    test_subclass()
     print("Everything worked as expected.")
